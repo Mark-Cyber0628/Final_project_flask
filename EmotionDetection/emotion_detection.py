fca2235c82fef1ffe5f44027772c82fb1f0804aa
Mark-Cyber0628 <szakalmark0628@gmail.com>
2025-12-26 07:59:05 -0500

    csomag hozzadva

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,56 @@
+"""
+emotion_detection.py
+"""
+
+import requests
+import json
+
+URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+
+def emotion_detector(text_to_analyze):
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(
+        URL,
+        headers=HEADERS,
+        json=payload,
+        timeout=10
+    )
+
+    response.raise_for_status()
+
+    # JSON szöveg -> dict
+    response_dict = json.loads(response.text)
+
+    emotions = response_dict["emotionPredictions"][0]["emotion"]
+
+    anger = emotions["anger"]
+    disgust = emotions["disgust"]
+    fear = emotions["fear"]
+    joy = emotions["joy"]
+    sadness = emotions["sadness"]
+
+    scores = {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness
+    }
+
+    dominant_emotion = max(scores, key=scores.get)
+
+    return {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness,
+        "dominant_emotion": dominant_emotion
+    }
