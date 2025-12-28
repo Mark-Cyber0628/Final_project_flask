90d13e98ecbebd30f03ffe2d69426e4a4d6e234b
Mark-Cyber0628 <szakalmark0628@gmail.com>
2025-12-28 04:20:22 -0500

    unittests

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,15 +1,15 @@
 """test_emotion_detection.py: egységtesztek a emotion_detector függvényhez"""
 
-from Final_project_flask.EmotionDetection.emotion_detection import emotion_detector
+from EmotionDetection import emotion_detector
 
 def run_tests():
-    test_cases = [
-        ("Örülök, hogy ez történt", "joy"),
-        ("Nagyon dühös vagyok emiatt", "anger"),
-        ("Már attól is undorodom, hogy hallok erről", "disgust"),
-        ("Nagyon szomorú vagyok emiatt", "sadness"),
-        ("Nagyon félek, hogy ez meg fog történni", "fear"),
-    ]
+    test_cases = test_cases = [
+    ("I am glad this happened", "joy"),
+    ("I am very angry about this", "anger"),
+    ("I am disgusted just hearing about this", "disgust"),
+    ("I am very sad about this", "sadness"),
+    ("I am very afraid that this will happen", "fear"),
+]
 
     for text, expected in test_cases:
         result = emotion_detector(text)
